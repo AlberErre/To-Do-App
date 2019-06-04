85f88f249e2a5da1b4575bcd13fdcff33339686e
Alber Erre <arblugo@gmail.com>
2019-06-04 23:28:42 +0200

    refactor - complete rebuild to handle EDIT on every note (Note.js)

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,23 +1,88 @@
 import React, { Component } from "react";
+import TitleInput from './TitleInput';
+import DescriptionInput from "./DescriptionInput";
 import "../App.css";
 
-const Note = (props) => {
-  return (
-    <div className="note">
-      <div className="note-title">
-        {props.noteInfo.name}
+class Note extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editMode: false,
+      noteId: null,
+      newName: null,
+      newDescription: null
+    };
+
+    this.startEdit = this.startEdit.bind(this);
+    this.finishEdit = this.finishEdit.bind(this);
+  }
+
+  startEdit(id) {
+    this.setState({
+      editMode: true,
+      noteId: id
+    });
+  }
+
+  finishEdit(event) {
+    event.preventDefault();
+    let newInfo = {
+      name: event.target.elements.noteTitle.value,
+      description: event.target.elements.noteText.value
+    };
+
+    this.props.updateNote(this.state.noteId, newInfo);
+    this.setState({
+      editMode: false,
+      newName: newInfo.name,
+      newDescription: newInfo.description
+    });
+  }
+
+  render() {
+
+    return (
+      <div>
+        {
+          !this.state.editMode &&
+          <div className="note">
+            <div className="note-title">
+              {(!this.state.newName) ? this.props.noteInfo.name : this.state.newName}
+            </div>
+            <div className="note-text">
+              {(!this.state.newDescription) ? this.props.noteInfo.description : this.state.newDescription}
+            </div>
+            <span onClick={() => this.startEdit(this.props.noteInfo.id)} className="note-edit">
+              edit
+            </span>
+            <span onClick={() => this.props.removeNote(this.props.noteInfo.id)} className="note-delete">
+              ×
+            </span>
+          </div>
+        }
+        {
+          this.state.editMode &&
+          <div className="note">
+            <form onSubmit={this.finishEdit}>
+              <div className="note-title">
+                <TitleInput
+                  currentValue={this.props.noteInfo.name}
+                />
+              </div>
+              <div className="note-text">
+                <DescriptionInput
+                  currentValue={this.props.noteInfo.description}
+                />
+              </div>
+              <button className="note-edit">
+                finish
+              </button>
+            </form>
+          </div>
+        }
       </div>
-      <div className="note-text">
-        {props.noteInfo.description}
-      </div>
-      <span className="note-edit">
-        edit
-      </span>
-      <span onClick={() => props.removeNote(props.noteInfo.id)} className="note-delete">
-        ×
-      </span>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Note;
