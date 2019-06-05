e6a9c3d658a4b0fbb38e0c1f6d9407b96d7c2ca0
Alber Erre <arblugo@gmail.com>
2019-06-05 12:51:08 +0200

    refactor - update descriptionInput to handle dynamic rows (DescriptionInput.js)

--- src/components/DescriptionInput.js
+++ src/components/DescriptionInput.js
@@ -3,13 +3,12 @@ import "../App.css";
 
 const DescriptionInput = (props) => {
   return (
-	<textarea
-    name="noteText"
-    placeholder={(props.currentValue) ? props.currentValue : 'Description...'}
-		cols="50"
-		rows="8"
-		maxLength="400">
-	</textarea>
+    <textarea
+      name="noteText"
+      placeholder={(props.currentValue) ? props.currentValue : 'Description...'}
+      className="descriptionInput"
+      rows={(props.customRows) ? props.customRows : '5'}>
+    </textarea>
   );
 };
 
